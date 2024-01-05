3bf26e1447120e27a592ac9df6d7d532a8bdb3ed
Vadim Tambovtsev <vadtam2@gmail.com>
2024-01-05 21:00:35 +0200

    second commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tokenizer (Test project)
+## What this app doess
+The app tokenizes a sentence.
--- tokenizer/Module/Main/MainPageViewController.swift
+++ tokenizer/Module/Main/MainPageViewController.swift
@@ -5,4 +5,11 @@
 //  Created by Vadim Tambovtsev on 05/01/2024.
 //
 
-import Foundation
+import UIKit
+
+final class MainPageViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("MainPageViewController:viewDidLoad")
+    }
+}
